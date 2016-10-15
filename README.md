c3c5e121fe1faf8a7ca31ec9548f7ec34377f38e
tomaka <htomaka@treeptik.fr>
2016-10-15 23:51:19 +0200

    playing with jsx

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# react-basics
-Playground for learning react.js
+# react-webpack
+Basic Webpack configuration for react.js
+
+## Install dependencies
+
+Run `npm install`
+
+## Build
+
+Run `npm run build`
+
+## Dev server
+
+Run `npm run start` to start the dev server then open http://localhost:3000
---
+++ app/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+var Profiles = React.createClass ({
+    getInitialState: function(){
+        return {
+            profiles: [
+                {name: 'David'},
+                {name: 'Sarah'}
+            ]
+        }
+    },
+    render: function () {
+        console.log(this.state);
+        return (
+            <section>
+                <article>
+                    <h3>User 1: {this.state.profiles[0].name}</h3>
+                </article>
+                <article>
+                <h3>User 2: {this.state.profiles[1].name}</h3>
+                </article>
+            </section>
+        )
+    }
+});
+var App = React.createClass ({
+    render: function () {
+        return (
+            <div>
+                <h1>Profiles</h1>
+                <Profiles/>
+            </div>
+        )
+    }
+});
+ReactDOM.render (
+    <App />,
+    document.getElementById ('root')
+);
